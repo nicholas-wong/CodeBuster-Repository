8ef04528b6e5664fdc0666808108a2ec94274542
davidgarza95 <31714089+davidgarza95@users.noreply.github.com>
2017-09-20 19:41:22 -0500

    Add files via upload

---
+++ imdb_queries.sql
@@ -0,0 +1,42 @@
+/* Query 1: Find all movies with Orlando Bloom sorted by year, from oldest to newest */
+
+/* Query 2: Find all directors whose movies start with the letter A, sorted alphabetically 
+by director name */
+
+/* Query 3: Find all movies whose rating is higher than 9.5 */
+
+/* Query 4: Find all movies whose star is Tom Cruise, sorted by rating (lowest to highest) */
+
+/* Query 5:  *Find all adult films whose writer's name starts with W, sorted by release date/
+
+/* Query 6: Find all soundtrack personnel in Hugh Jackman movies, sorted by runtime minutes (shortest to longest) */
+
+/* Query 7: Find all TV Episodes that aired after 2007, organized by year released (earliest to latest) */
+
+/* Query 8: Find all drama movies */
+
+/* Query 9: Find all producers who produced comedy movies starring Adam Sandler */
+
+/* Query 10: Find all actresses who have starred in mystery TV series that ended in 2005, sorted alphabetically */
+
+/* Query 11: Find all shorts that start with the letter 'X' */
+
+/* Query 12: Find all directors that have directed a short longer than 30 minutes, sorted by runtime */
+
+/* Query 13: Find all series that have more than 7 seasons */
+
+/* Query 14: Find all movies that star Johnny Depp and Helena Bonham Carter */
+
+/* Query 15: Find all writers that worked with James Cameron */
+
+/* Query 16: Find all soundtrack engineers that have worked in comedy tv series, ordered by rating (lowest to highest) */
+
+/* Query 17: Find all news TV episodes aired in 2011 */
+
+/* Query 18: Find all actors that have starred in adult films */
+
+/* Query 19: Find all non-adult films that have more than 1000 votes on IMDB, sorted by rating */
+
+/* Query 19: Find all TV movies released in 1995 that rated lower than a 1, sorted by rating (lowest to highest)*/
+
+/* Query 20: Find all TV movies whose original title differs from its primary title  */
